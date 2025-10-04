588867d885009024864cbd237245fac3f6a024ca
JorgeAHernandezC80 <jotache80@icloud.com>
2025-10-04 07:15:04 -0500

    El paquete modelo agrega interfaz LiquidadorInteres para cálculo de intereses.

--- src/modelo/LiquidadorInteres.java
+++ src/modelo/LiquidadorInteres.java
@@ -1,9 +1,6 @@
 package modelo;
 
-// Interfaz funcional para calcular intereses en una cuenta bancaria
 @FunctionalInterface
 public interface LiquidadorInteres {
-
-    // Metodo abstracto para calcular el interés basado en el saldo
     double calcular(double saldo);
 }
